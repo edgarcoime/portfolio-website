508062a08c46a580c620757adeb8c366c4a86f71
Edgar Coime <e.coimeii@gmail.com>
2022-03-13 13:34:58 -0700

    feat: setup components

--- client/postcss.config.cjs
+++ client/postcss.config.cjs
@@ -1,6 +1,18 @@
-module.exports = {
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
-  },
+const tailwindcss = require("tailwindcss");
+const autoprefixer = require("autoprefixer");
+
+const config = {
+  plugins: [
+    tailwindcss(),
+    autoprefixer,
+  ]
 }
+
+module.exports = config;
+
+// module.exports = {
+//   plugins: {
+//     tailwindcss: {},
+//     autoprefixer: {},
+//   },
+// }
--- client/src/routes/index.svelte
+++ client/src/routes/index.svelte
@@ -1,3 +1,23 @@
-<h1 class="text-3xl font-bold underline bg-red-500">
-  Hello world!
-</h1>
+<script lang="ts">
+import About from "$lib/components/about.svelte";
+import Footer from "$lib/components/footer.svelte";
+import Header from "$lib/components/header.svelte";
+import Navbar from "$lib/components/navbar.svelte";
+import Skills from "$lib/components/skills.svelte";
+import Testimonial from "$lib/components/testimonial.svelte";
+import Work from "$lib/components/work.svelte";
+
+console.log("Hello world!");
+</script>
+
+<h1 class="text-3xl font-bold underline bg-red-500">Portfolio!</h1>
+
+<div>
+  <Navbar />
+  <Header />
+  <About />
+  <Work />
+  <Skills />
+  <Testimonial />
+  <Footer />
+</div>
